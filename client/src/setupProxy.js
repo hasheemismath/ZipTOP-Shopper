e0d97f4933261dc689f5e64a157b9c8a390c7ea8
hasheemismath <hasheemhush@gmail.com>
2020-06-16 21:05:57 +0530

    server file updated

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:3002',
+            target: process.env.PORT || 'http://localhost:3002',
             changeOrigin: true,
         })
     );
