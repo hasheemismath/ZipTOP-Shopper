36849443692bfe1c7a313adcbc5c1ba95a435eb4
hasheemismath <hasheemhush@gmail.com>
2020-06-16 22:59:48 +0530

    client 2 update

---
+++ client2/build/precache-manifest.1bf10a82c28007dd76009e223af35ead.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cb3382b9a19704dff2262c6e0bb9f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bdb84461265409c4144",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "2005a7c7718420ec8d9d",
+    "url": "/static/css/main.976c8738.chunk.css"
+  },
+  {
+    "revision": "9bdb84461265409c4144",
+    "url": "/static/js/2.bb12979b.chunk.js"
+  },
+  {
+    "revision": "94e9253e40d7d044df5d436478dc845f",
+    "url": "/static/js/2.bb12979b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2005a7c7718420ec8d9d",
+    "url": "/static/js/main.ed9f82f5.chunk.js"
+  },
+  {
+    "revision": "fd68e0663feb8c8bb089",
+    "url": "/static/js/runtime-main.abdf9530.js"
+  }
+]);
