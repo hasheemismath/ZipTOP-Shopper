cc4f2cc3e130ddff622ac3e1b097995a33a7c840
hasheemismath <hasheemhush@gmail.com>
2020-06-17 22:36:54 +0530

    resources added etc

--- client/build/precache-manifest.15685c3b072342a3ddd5e8145c4bae74.js
+++ client/build/precache-manifest.15685c3b072342a3ddd5e8145c4bae74.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fd2bf8b98a5265d8830492b648cff99c",
+    "revision": "7ff72e1baad31cc0b20b5ce7fe992e93",
     "url": "/index.html"
   },
   {
-    "revision": "6a9e00b9534bb1f26be5",
+    "revision": "de62807ecaea4b62e541",
     "url": "/static/css/2.9522b9fe.chunk.css"
   },
   {
-    "revision": "fda9c11edf30ac551aaa",
+    "revision": "a279cbd37240a3a2e787",
     "url": "/static/css/main.976c8738.chunk.css"
   },
   {
-    "revision": "6a9e00b9534bb1f26be5",
-    "url": "/static/js/2.c3288d4d.chunk.js"
+    "revision": "de62807ecaea4b62e541",
+    "url": "/static/js/2.8e0681ec.chunk.js"
   },
   {
     "revision": "94e9253e40d7d044df5d436478dc845f",
-    "url": "/static/js/2.c3288d4d.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8e0681ec.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fda9c11edf30ac551aaa",
-    "url": "/static/js/main.64fbf457.chunk.js"
+    "revision": "a279cbd37240a3a2e787",
+    "url": "/static/js/main.3a4f48aa.chunk.js"
   },
   {
     "revision": "13c5ce7c89dba05c4fba",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fd3675a18350d890b2ff41091c587f1.js"
+  "/precache-manifest.15685c3b072342a3ddd5e8145c4bae74.js"
 );
 
 self.addEventListener('message', (event) => {
