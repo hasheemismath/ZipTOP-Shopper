5ecaea1beb7fa7bf42cc112a4c5e0d218b159350
hasheemismath <hasheemhush@gmail.com>
2020-06-17 15:03:01 +0530

    bUILD FORLDER ADDED

---
+++ client/build/precache-manifest.19f26cd0491302c0bd5c690233c1650c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf154096c8d87875798b5425e89e2995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a9e00b9534bb1f26be5",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "dd85e57ecf420d996f0b",
+    "url": "/static/css/main.976c8738.chunk.css"
+  },
+  {
+    "revision": "6a9e00b9534bb1f26be5",
+    "url": "/static/js/2.c3288d4d.chunk.js"
+  },
+  {
+    "revision": "94e9253e40d7d044df5d436478dc845f",
+    "url": "/static/js/2.c3288d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd85e57ecf420d996f0b",
+    "url": "/static/js/main.b9ff5998.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
