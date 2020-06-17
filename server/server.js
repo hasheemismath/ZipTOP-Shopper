8e3a955bff068360b3daf89411dd376fa2e05b93
hasheemismath <hasheemhush@gmail.com>
2020-06-17 14:57:37 +0530

    finished client

--- server/server.js
+++ server/server.js
@@ -437,12 +437,12 @@ app.post('/api/site/site_data',auth,admin,(req,res)=>{
 })
 
 // DEFAULT
-if( process.env.NODE_ENV === 'production' ){
+
     const path = require('path');
     app.get('/*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'../client2','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
     })
-}
+
 
 const port = process.env.PORT || 3002
 
